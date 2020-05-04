5ec8b2e2f648159d6a9257562269a4c25cde3447
Woober <ddelfaus@gmail.com>
2020-05-04 18:25:01 -0400

    p2 counting valleys

---
+++ HackerRank/Easy/Couting Valleys.py
@@ -0,0 +1,22 @@
+#plan
+# go through arr and check if it is below start
+#if it is below start then contiune going though arr untill it goes back at sea level
+#when it does incrase valley by 1
+
+
+def countingValleys(n, s):
+    altitude = 0
+    valleys = 0
+    for x in s:
+        if x == 'U':
+            altitude = altitude + 1
+        else:
+            altitude = altitude - 1
+        
+        if x == 'U' and altitude == 0:
+            valleys = valleys + 1
+    
+    return valleys                
+
+
+print(countingValleys(5,'UDDDUDUU'))
