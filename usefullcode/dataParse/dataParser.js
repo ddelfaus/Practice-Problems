1fcb3cac0e52d4bceea95808faa5bd4ae3b2eca6
woober <ddelfaus@gmail.com>
2021-02-08 17:53:00 -0500

    dp

--- usefullcode/dataParse/dataParser.js
+++ usefullcode/dataParse/dataParser.js
@@ -18,3 +18,52 @@ function parseData(data){
     }
 
     return dataObject
+
+
+
+
+
+    
+]
+
+console.log(new Date(1612362600), "Date")
+
+useEffect(() => {
+    axios
+    .get(`http://192.168.1.7:62500/api/stocks/GOOG/minutes`)
+    .then(res =>{
+        console.log(res.data, "data")
+       
+        // parseData(res.data,dataArray)
+        // console.log(stockData, "stock data")
+        // console.log(dataArray, "efaa")
+    })  
+    .catch(err => {
+        console.log("There was an error, ", err)
+    })
+
+}, []);
+
+
+
+
+return (
+    <>
+        <VictoryChart
+            theme={VictoryTheme.material}
+            domainPadding={{ x: 25 }}
+            scale={{ x: "time" }}
+        >
+        <VictoryAxis tickFormat={(t) => `${t.getDate()}/${t.getMonth()}`}/>
+        <VictoryAxis dependentAxis/>
+        <VictoryCandlestick
+            candleColors={{ positive: "#5f5c5b", negative: "#c43a31" }}
+            data={data}
+        />
+        </VictoryChart>
+     
+    </>
+
+)
+}
+
